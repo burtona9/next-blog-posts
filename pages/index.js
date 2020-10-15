35fe8181a8046de2a7d2928e63fa391ef53a19fc
burtona9 <a.burton@dorsetcc.gov.uk>
2020-10-15 22:18:57 +0100

    removed more layout

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,16 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import  { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home({ allPostsData }) {
+export default function Home {
   return (
     <>
       <Head>
-        <title>{siteTitle}</title>
+        <title>test</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
 
-        <Link href="/posts/first-post">
-          <a>Posts</a>
-        </Link>
+    
       </section>
     </>
   )
